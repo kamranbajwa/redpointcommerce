c47e491d2eb29b97f0d7461a0e796ba1432018c8
Shoaib <shoaib@gems.techverx.com>
2015-04-13 13:29:30 +0500

    spree logo

--- db/migrate/20150413075340_add_columns_to_templates.rb
+++ db/migrate/20150413075340_add_columns_to_templates.rb
@@ -1,6 +1,6 @@
 class AddColumnsToTemplates < ActiveRecord::Migration
   def change
-  	add_column :spree_slider_images, :template_id, :integer
+  	# add_column :spree_slider_images, :template_id, :integer
   	add_column :spree_logos, :template_id, :integer
   	add_index :spree_favicons, :template_id
   	add_index :spree_slider_images, :template_id
