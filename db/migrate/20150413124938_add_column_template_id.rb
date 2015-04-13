b58d5f42cc5864ae6416f580462c6244bd9ef7d8
Shoaib <shoaib@gems.techverx.com>
2015-04-13 17:51:50 +0500

    changes

--- db/migrate/20150413124938_add_column_template_id.rb
+++ db/migrate/20150413124938_add_column_template_id.rb
@@ -1,5 +1,12 @@
 class AddColumnTemplateId < ActiveRecord::Migration
   def change
   	add_column :spree_slider_images, :template_id, :integer
+  	Spree::Template.all.each do |temp| 
+  Spree::Logo.create(:avatar_file_name=>"logo.png",:avatar_content_type=> "image/png", :template_id => temp.id)
+  Spree::Favicon.create(:favicon_image_file_name=>"logo.ico",:favicon_image_content_type=> "image/x-icon", :template_id => temp.id)
+  (0..4).each do |a|
+  Spree::SliderImage.create(:slider_image_file_name=>"slider#{a}.png",:slider_image_content_type=> "image/png",:template_id => temp.id)
+end 
+end 
   end
 end
