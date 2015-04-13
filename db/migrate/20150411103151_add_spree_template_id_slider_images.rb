aac94bb9a61e7637c34364de0f4361e5c96be8ae
Shoaib <shoaib@gems.techverx.com>
2015-04-13 12:31:00 +0500

    integer speeling mistake

--- db/migrate/20150411103151_add_spree_template_id_slider_images.rb
+++ db/migrate/20150411103151_add_spree_template_id_slider_images.rb
@@ -1,7 +1,7 @@
 class AddSpreeTemplateIdSliderImages < ActiveRecord::Migration
   def change
-  	add_column :spree_slider_images, :template_id, :integre
-  	add_column :spree_logos, :template_id, :integre
+  	add_column :spree_slider_images, :template_id, :integer
+  	add_column :spree_logos, :template_id, :integer
   end
 end
 
