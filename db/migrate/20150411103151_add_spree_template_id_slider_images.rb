beb25b209fa61a661152c76ac1b628b7c91f478d
Shoaib <shoaib@gems.techverx.com>
2015-04-13 12:55:34 +0500

    changes in columns

--- db/migrate/20150411103151_add_spree_template_id_slider_images.rb
+++
@@ -1,8 +0,0 @@
-class AddSpreeTemplateIdSliderImages < ActiveRecord::Migration
-  def change
-  	add_column :spree_slider_images, :template_id, :integer
-  	add_column :spree_logos, :template_id, :integer
-  	
-  end
-end
-
