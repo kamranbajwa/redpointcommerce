dd3c69a5d196119508083c9c1ac3954bf298204d
Shoaib <shoaib@gems.techverx.com>
2015-04-21 21:58:31 +0500

    slider image max size

--- app/models/spree/slider_image.rb
+++ app/models/spree/slider_image.rb
@@ -10,5 +10,6 @@ class SliderImage < Spree::Base
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :slider_image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
+  validates_attachment_size :slider_image, :less_than => 1.megabytes
 end
 end
