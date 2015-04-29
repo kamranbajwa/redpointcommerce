9023fcf1c7f79d6bfe321bbca23c8feeab8d1c42
Shoaib <shoaib@gems.techverx.com>
2015-04-29 15:55:51 +0500

    g map edit

--- app/assets/javascripts/g_map.js
+++ app/assets/javascripts/g_map.js
@@ -2,15 +2,16 @@ var geocoder;
        var map;
        var markersArray = [];
         var locationsArray=[]
-      
+
 
    function initialize() 
-    {
+    { console.log(locationsArray)
         geocoder = new google.maps.Geocoder();
 
     latlang = geocoder.geocode( { 
 
-           'address': 'Islmabad, Pakistan'},                                             
+           'address': locationsArray[0]
+         },                                             
 
             function(results, status) 
     {  
@@ -33,13 +34,10 @@ var geocoder;
 
           var myOptions = 
           {
-                      center: latlang, zoom: 5, 
-          mapTypeId: google.maps.MapTypeId.SATELLITE,
-                      navigationControlOptions: 
-          {
-                   style: google.maps.NavigationControlStyle.SMALL
-                      }
-                      };
+            center: latlang, 
+            zoom: 5, 
+         
+          };
           map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
                      plotMarkers();
                     }
@@ -51,10 +49,12 @@ var geocoder;
         function plotMarkers(){
           for(var i = 0; i < locationsArray.length; i++){
             codeAddresses(locationsArray[i]);
+            //marker.setTitle("aas");
           }
         }
 
          function codeAddresses(address){
+          console.log('ad'+address)
          geocoder.geocode( { 'address': address}, function(results, status) { 
          if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
