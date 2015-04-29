6fa34727552c5895bc83c7f3e85a3bdaef1fa3ca
Shoaib <shoaib@gems.techverx.com>
2015-04-29 18:43:56 +0500

    drope table for qubaish

--- db/migrate/20150429092546_create_spree_themes.rb
+++ db/migrate/20150429092546_create_spree_themes.rb
@@ -1,9 +1,10 @@
 class CreateSpreeThemes < ActiveRecord::Migration
   def change
-    # create_table :spree_themes do |t|
-    # 	t.string :name
-    # 	t.integer :template_id
-    #   t.timestamps
-    # end
+  	drop_table :spree_themes
+    create_table :spree_themes do |t|
+    	t.string :name
+    	t.integer :template_id
+      t.timestamps
+    end
   end
 end
