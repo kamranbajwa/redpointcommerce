50c0b4cc664509e85e8c579a0d03fbd97ac2656e
Shoaib <shoaib@gems.techverx.com>
2015-05-08 12:11:27 +0500

    csv changes

--- app/models/spree/import.rb
+++ app/models/spree/import.rb
@@ -3,14 +3,14 @@ class Import < Spree::Base
 	def self.prodcut_import(file)
   		CSV.foreach(file.path, headers: true) do |row|
   			product_params=row.to_hash
+  			pr=Spree::Product.new(product_params)
   			if product_params['images_url']
   			file_path=product_params['images_url']
   			product_params.delete("images_url")
   			file_path=File.open(file_path)
   			imgs=Spree::Image.create!(:attachment=>file_path)
+  			pr.images << imgs
   			end
-    		pr=Spree::Product.new(product_params)
-    		pr.images << imgs
     		pr.save!
     	
   		end
