f6e597edf0b2b5d16c8657bf5bb2e112a336a6c0
Shoaib <shoaib@gems.techverx.com>
2015-05-12 13:53:25 +0500

    postal type change

--- db/migrate/20150512065524_postal_code_type_change.rb
+++ db/migrate/20150512065524_postal_code_type_change.rb
@@ -1,6 +1,6 @@
 class PostalCodeTypeChange < ActiveRecord::Migration
   def change
   	
-  	change_column :spree_organizations, :postal_code, :string
+  	change_column :spree_org_addresses, :postal_code, :string
   end
 end
