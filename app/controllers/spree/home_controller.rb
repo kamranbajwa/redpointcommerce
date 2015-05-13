36dda2de871bf7a33cb392a411dfe9d75d39a3e6
Shoaib <shoaib@gems.techverx.com>
2015-05-13 13:28:20 +0500

    recumnended products

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -7,8 +7,9 @@ module Spree
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products
       @taxonomies = Spree::Taxonomy.all
+      @recommended_product=@products.where(:is_recommended=>true)
 
-      render :template => "/spree/home/#{@selected_template.template_no}/index.html.erb", :locals => {:products => @products,:taxonomies => @taxonomies, :searcher => @searcher }
+      render :template => "/spree/home/#{@selected_template.template_no}/index.html.erb", :locals => {:products => @products,:taxonomies => @taxonomies, :searcher => @searcher,:recommended_product => @recommended_product}
     end
   end
 end
