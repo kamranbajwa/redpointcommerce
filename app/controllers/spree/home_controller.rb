64f33a82cd34dd80d541f57c303a89df1ff73cf8
Shoaib <shoaib@gems.techverx.com>
2015-05-15 12:30:37 +0500

    widgets for warehoue

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -7,8 +7,8 @@ module Spree
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products
       @taxonomies = Spree::Taxonomy.all
-      @recommended_products=@products.where(:is_recommended=>true)
-      @best_selling_products=Spree::Product.all.includes(:orders).sort_by { |p| p.orders.size }.reverse.take(3)
+      @recommended_products=Spree::Product.where(:is_recommended=>true).limit 5
+      @best_selling_products=Spree::Product.all.includes(:orders).sort_by { |p| p.orders.size }.reverse.take(5)
       render :template => "/spree/home/#{@selected_template.template_no}/index.html.erb", :locals => {:products => @products,:taxonomies => @taxonomies, :searcher => @searcher,:recommended_products => @recommended_products,:best_selling_products => @best_selling_products }
     end
   end
