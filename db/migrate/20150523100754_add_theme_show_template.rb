a492bb8e539b76ac145d176696ad6133383e1ae3
Rao Bilal <rao.bilal@gems.techverx.com>
2015-05-23 19:02:16 +0500

    by shoaib,bilal sytems merge

--- db/migrate/20150523100754_add_theme_show_template.rb
+++ db/migrate/20150523100754_add_theme_show_template.rb
@@ -1,4 +1,4 @@
 class AddThemeShowTemplate < ActiveRecord::Migration
- 	tmp=Spree::Template.find_by_template_no 4
+ 	tmp=Spree::Template.find_by_template_no "4"
  	Spree::Theme.create(name: "default", template_id: tmp.try(:id))
 end
