d2d050e76fdfced067e2823567dfa689af1c76e3
Qubaish Bhatti <qubaish@gems.techverx.com>
2015-05-23 20:03:12 +0500

    reponsive issues

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -32,7 +32,7 @@ attachment_config = {
   styles: {
       mini:     "48x48>",
       small:    "100x100>",
-      product:  "240x240>",
+      product:  "240x240!",
       large:    "600x600>",
        logo: '240x75#',
     square: '200x200#',
