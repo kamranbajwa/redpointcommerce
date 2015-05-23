c5d186d72ddd204fcb5bf231d26565d21f7b9a23
Your Name <techverx@techverx-Lenovo-G50-70>
2015-05-23 15:56:44 +0500

    slider issues

--- app/models/spree/slider_image.rb
+++ app/models/spree/slider_image.rb
@@ -3,7 +3,7 @@ class SliderImage < Spree::Base
 	belongs_to :template
 	has_attached_file :slider_image, styles: {
     medium: '300x300!', 
-    xlarge:  '1280x879!'
+    xlarge:  '1000x490>'
   }
 
   # Validate the attached image is image/jpg, image/png, etc
