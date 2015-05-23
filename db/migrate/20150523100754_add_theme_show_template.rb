f68e419b1712ac7f841fa051578a11ff5b69e8df
Your Name <techverx@techverx-Lenovo-G50-70>
2015-05-23 19:05:33 +0500

    migration file

--- db/migrate/20150523100754_add_theme_show_template.rb
+++ db/migrate/20150523100754_add_theme_show_template.rb
@@ -1,4 +1,12 @@
 class AddThemeShowTemplate < ActiveRecord::Migration
- 	tmp=Spree::Template.find_by_template_no 4
- 	Spree::Theme.create(name: "default", template_id: tmp.try(:id))
+ 	
+  tmp = Spree::Template.where("template_no = ? ", "4").first
+  
+  st = Spree::Theme.new
+  st.name = "default"
+  st.template_id = tmp.id
+  st.save
+  
+#  tmp=Spree::Template.find_by_template_no 4
+# 	Spree::Theme.create(name: "default", template_id: tmp.try(:id))
 end
