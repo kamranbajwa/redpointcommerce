0b28e050d7b7d78f9a21da8b7ea916eb1eb52f60
Your Name <techverx@techverx-Lenovo-G50-70>
2015-05-23 12:16:13 +0500

    update the issues

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -1,5 +1,5 @@
 module Spree
-  class Spree::Blog < Spree::Base
+  class Blog < Spree::Base
     acts_as_taggable
     has_attached_file :avatar, :styles => { :blarge =>"700x500!",:medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
