d19431fd0ee2cbe0b8217607ed809d2ac50d691c
Your Name <techverx@techverx-Lenovo-G50-70>
2015-05-25 15:33:44 +0500

    content type

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -2,6 +2,7 @@ module Spree
   class Blog < Spree::Base
     acts_as_taggable
     has_attached_file :avatar, :styles => { :blarge =>"700x500!", :xthumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+   
+  validates_attachment_content_type :slider_image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
   end
 end
