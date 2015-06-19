176c4cba5d7e44898cedea1ec676487023693954
Qubaish Bhatti <qubaish@gems.techverx.com>
2015-06-19 16:55:36 +0500

    revert changes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,2 @@
-# strip = Spree::Gateway::StripeGateway.where(:active=>true, :environment=>Rails.env).last
-# Stripe.api_key = strip[:preferences][:secret_key]
-# #STRIPE_PUBLIC_KEY = strip[:preferences][:publishable_key]
-#   Rails.configuration.stripe = {
-#       :publishable_key => strip[:preferences][:publishable_key],
-#       :secret_key      => strip[:preferences][:secret_key]
-#     }
-
-#     Stripe.api_key = Rails.configuration.stripe[:secret_key]
- #Spree::PaymentMethod.find_by_name "
+Stripe.api_key = "sk_test_je98XVAYSwxGfKKvXQrIhsas"
+STRIPE_PUBLIC_KEY = "pk_test_YiakAH8aEIK46o2VGwQ2h9O9"
