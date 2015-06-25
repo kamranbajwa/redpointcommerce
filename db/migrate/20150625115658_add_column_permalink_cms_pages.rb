53a830c8375e1494e64b91a6e8cd7937e6f1dff5
Shoaib <shoaib@gems.techverx.com>
2015-06-25 19:55:06 +0500

    revert some changes

--- db/migrate/20150625115658_add_column_permalink_cms_pages.rb
+++ db/migrate/20150625115658_add_column_permalink_cms_pages.rb
@@ -1,6 +1,6 @@
 class AddColumnPermalinkCmsPages < ActiveRecord::Migration
   # for older record to create frienly id
-  Spree::CmsPage.find_each(&:save)
+  # Spree::CmsPage.find_each(&:save)
   def change
   	add_column :spree_cms_pages, :slug, :string
   end
