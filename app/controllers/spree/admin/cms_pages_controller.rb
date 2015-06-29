ca1c849da9a0e75c6df8f23f637fea581568cd10
Qubaish Bhatti <qubaish@gems.techverx.com>
2015-06-29 14:23:31 +0500

    widgets functionality

--- app/controllers/spree/admin/cms_pages_controller.rb
+++ app/controllers/spree/admin/cms_pages_controller.rb
@@ -61,7 +61,6 @@ class Spree::Admin::CmsPagesController < Spree::Admin::ResourceController
    end
 
    def cms_page_params
-  
       params.require(:cms_page).permit(:title, :description, :template_id, :meta_tags, :meta_description, :tag_list, :sort, :default_flag, :static_flag, :page_id, :avatar)
    end
 
