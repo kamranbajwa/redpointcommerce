e37a524733687b131df102e37c995877a518dbe7
Shoaib <shoaib@gems.techverx.com>
2015-07-28 12:54:11 +0500

    versions for porudcts

--- app/controllers/spree/admin/versions_controller.rb
+++ app/controllers/spree/admin/versions_controller.rb
@@ -14,7 +14,7 @@ def products_versions
 end
 def show_product_version
 	@p=Spree::Product.friendly.find(params[:product_id])
-	@variants=Spree::Variant.where(id: @p.id)
+	@variants=Spree::Variant.where(product_id: @p.id)
 	@price=@variants.nil? ? @variants.first.prices : nil
 end
 def users_versions	
