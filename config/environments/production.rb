eaa25d6ba7a43515b98450b11be34e6d30487959
Qubaish Bhatti <qubaish@gems.techverx.com>
2015-08-08 14:00:23 +0500

    changes in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,18 +22,18 @@ SpreeExample::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.initialize_on_precompile = false
+  # config.assets.compile = false
+  # config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  # config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.7.1'
+  # config.assets.version = '1.7.1'
 
   # Precompile additional assets
   config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
