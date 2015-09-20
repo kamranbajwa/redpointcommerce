9e59c098610ce273106bb921f6f48bee80a4597d
SamuelDavis <sam9@cfu.net>
2015-09-20 17:16:36 -0400

    comment out stripe because it was throwing error

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,12 +1,12 @@
- if ActiveRecord::Base.connection.table_exists? 'spree_payment_methods'
-strip = Spree::Gateway::StripeGateway.where(:active=>true, :environment=>Rails.env).last
-Stripe.api_key = strip[:preferences][:secret_key]
-#STRIPE_PUBLIC_KEY = strip[:preferences][:publishable_key]
-  Rails.configuration.stripe = {
-      :publishable_key => strip[:preferences][:publishable_key],
-      :secret_key      => strip[:preferences][:secret_key]
-    }
+#  if ActiveRecord::Base.connection.table_exists? 'spree_payment_methods'
+# strip = Spree::Gateway::StripeGateway.where(:active=>true, :environment=>Rails.env).last
+# Stripe.api_key = strip[:preferences][:secret_key]
+# #STRIPE_PUBLIC_KEY = strip[:preferences][:publishable_key]
+#   Rails.configuration.stripe = {
+#       :publishable_key => strip[:preferences][:publishable_key],
+#       :secret_key      => strip[:preferences][:secret_key]
+#     }
 
-    Stripe.api_key = Rails.configuration.stripe[:secret_key]
- #Spree::PaymentMethod.find_by_name "
-end
+#     Stripe.api_key = Rails.configuration.stripe[:secret_key]
+#  #Spree::PaymentMethod.find_by_name "
+# end
