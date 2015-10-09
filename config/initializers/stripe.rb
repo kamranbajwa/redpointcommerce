4d7d4dc83df20b8fd8874737f948119af72d61ba
shoaib <shoaib@gems.techverx.com>
2015-10-09 13:38:06 +0500

    strip key issue resolved

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,6 @@
  if ActiveRecord::Base.connection.table_exists? 'spree_payment_methods'
 strip = Spree::Gateway::StripeGateway.where(:active=>true, :environment=>Rails.env).last
+if strip
 Stripe.api_key = strip[:preferences][:secret_key]
 #STRIPE_PUBLIC_KEY = strip[:preferences][:publishable_key]
   Rails.configuration.stripe = {
@@ -9,4 +10,5 @@ Stripe.api_key = strip[:preferences][:secret_key]
 
     Stripe.api_key = Rails.configuration.stripe[:secret_key]
  #Spree::PaymentMethod.find_by_name "
+end
 end
